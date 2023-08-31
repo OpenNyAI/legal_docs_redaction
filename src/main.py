28c114525d5c08c97c409a82fd95781991313686
prathamesh <prathamk@thoughtworks.com>
2023-08-31 17:46:35 +0530

    made the intermediate file format as docx

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ import os
 
 if __name__ == "__main__":
     input_pdfs_folder = '/Users/prathamesh/tw_projects/OpenNyAI/data/LLM/Contracts for Review'
-    text_folder_path = os.path.join(input_pdfs_folder, 'output', 'text')
+    docx_folder_path = input_pdfs_folder + '_docx'
 
-    docx_converter = ConvertToDocx(input_pdfs_folder, text_folder_path)
+    docx_converter = ConvertToDocx(input_pdfs_folder, docx_folder_path)
     docx_converter.convert_to_docx()
 
-    redacted_output_path = os.path.join(input_pdfs_folder, 'output', 'text','redacted')
-    n = DataRedaction(text_folder_path, redacted_output_path)
+    redacted_output_path = docx_folder_path + '_redacted'
+    n = DataRedaction(docx_folder_path, redacted_output_path)
     n.redact_all_files_in_folder()
